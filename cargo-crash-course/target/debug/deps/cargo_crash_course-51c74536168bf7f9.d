7ce0fd1765ffe8f399507acdcca1012fa86bc4d3
DaftOdyssey <daftodyssey@gmail.com>
2022-08-11 21:01:15 -0500

    Starting Rust Course

---
+++ cargo-crash-course/target/debug/deps/cargo_crash_course-51c74536168bf7f9.d
@@ -0,0 +1,5 @@
+c:\Users\Dafto\rust_crash_course\rust-crash-course\cargo-crash-course\target\debug\deps\cargo_crash_course-51c74536168bf7f9.rmeta: src\main.rs
+
+c:\Users\Dafto\rust_crash_course\rust-crash-course\cargo-crash-course\target\debug\deps\cargo_crash_course-51c74536168bf7f9.d: src\main.rs
+
+src\main.rs:
---
+++ cargo-crash-course/target/debug/deps/cargo_crash_course-cb7626194683c002.d
@@ -0,0 +1,5 @@
+c:\Users\Dafto\rust_crash_course\rust-crash-course\cargo-crash-course\target\debug\deps\cargo_crash_course-cb7626194683c002.rmeta: src\main.rs
+
+c:\Users\Dafto\rust_crash_course\rust-crash-course\cargo-crash-course\target\debug\deps\cargo_crash_course-cb7626194683c002.d: src\main.rs
+
+src\main.rs:
